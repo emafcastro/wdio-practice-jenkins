1b72aa78b2f139802a00e3e84e06cb2ee4e40266
Emanuel Castro <castrofemanuel@gmail.com>
2023-10-11 04:11:37 -0700

    Adding chrome and firefox browsers in wdio.config

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,12 +52,17 @@ export const config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
-        browserName: 'MicrosoftEdge',
-        'ms:edgeOptions': {
-            args: ['--no-sandbox', '--disable-dev-shm-usage']
-        }
-    }],
+    capabilities: [
+        {
+            browserName: 'chrome',
+        }, {
+            browserName: 'firefox',
+        }, {
+            browserName: 'MicrosoftEdge',
+            'ms:edgeOptions': {
+                args: ['--no-sandbox', '--disable-dev-shm-usage']
+            }
+        }],
 
     //
     // ===================
